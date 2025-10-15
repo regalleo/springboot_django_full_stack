8bf0349aa77844b5c489c1f702be165a930e6256
Raj Shekhar Singh <115339056+regalleo@users.noreply.github.com>
2025-10-15 08:09:08 +0530

    TaskController.java

--- taskmanager/src/main/java/com/example/taskmanager/controller/TaskController.java
+++ taskmanager/src/main/java/com/example/taskmanager/controller/TaskController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
+@CrossOrigin(origins = "https://springboot-django-full-stack-2.onrender.com")
 public class TaskController {
 
     private final TaskService taskService;

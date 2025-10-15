561e8497d84e6287c8baaffb97086157e97c4218
Raj Shekhar Singh <115339056+regalleo@users.noreply.github.com>
2025-10-15 07:08:56 +0530

    settings.py

--- task_frontend/task_frontend/settings.py
+++ task_frontend/task_frontend/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 import os
 import environ  # make sure this is imported
 
+BACKEND_API_URL = os.environ.get('BACKEND_API_URL', 'http://localhost:8080')
+
 # 1️⃣ Define BASE_DIR first
 BASE_DIR = Path(__file__).resolve().parent.parent
 
